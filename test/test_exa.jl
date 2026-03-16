70c8731ec679a6357fbffe6b5451ac10d4167597
Klamkin, Michael <mklamkin3@gatech.edu>
2026-03-16 18:34:13 -0400

    fix examodels test

--- test/test_exa.jl
+++ test/test_exa.jl
@@ -11,7 +11,7 @@ using NLPModels, LinearAlgebra, ExaModels, KernelAbstractions
         x = ExaModels.variable(c, 2)
         ExaModels.objective(c, x[1]^2 + x[2]^2 + p[1] * x[1])
         ExaModels.constraint(c, x[1] + x[2] - p[2])
-        return ExaModel(c)
+        return ExaModel(c; prod = !isnothing(backend))
     end
 
     function _solve_exa(p_vals; backend = nothing, madnlp_opts = (;))
@@ -33,6 +33,9 @@ using NLPModels, LinearAlgebra, ExaModels, KernelAbstractions
         ))
     end
 
+    _to_device(v, ::Nothing) = v
+    _to_device(v, backend) = KernelAbstractions.copyto!(KernelAbstractions.allocate(backend, eltype(v), size(v)...), v)
+
     for (backend_name, backend, madnlp_opts) in backends
         @testset "backend=$backend_name" begin
             solver0 = _solve_exa(p0; backend, madnlp_opts)
@@ -43,22 +46,22 @@ using NLPModels, LinearAlgebra, ExaModels, KernelAbstractions
 
             for j in 1:n_p
                 Δp    = zeros(n_p); Δp[j] = 1.0
-                jvp   = MadDiff.jacobian_vector_product!(sens, Δp)
+                jvp   = MadDiff.jacobian_vector_product!(sens, _to_device(Δp, backend))
                 s_plus = _solve_exa(p0 .+ h .* Δp; backend, madnlp_opts)
-                @test isapprox(jvp.dx, (Vector(MadNLP.variable(s_plus.x)) .- x0) ./ h; atol)
-                @test isapprox(jvp.dy, (Vector(s_plus.y)                  .- y0) ./ h; atol)
+                @test isapprox(Vector(jvp.dx), (Vector(MadNLP.variable(s_plus.x)) .- x0) ./ h; atol)
+                @test isapprox(Vector(jvp.dy), (Vector(s_plus.y)                  .- y0) ./ h; atol)
             end
 
             rng   = MersenneTwister(42)
-            dL_dx = randn(rng, length(x0))
-            dL_dy = randn(rng, length(y0))
+            dL_dx = _to_device(randn(rng, length(x0)), backend)
+            dL_dy = _to_device(randn(rng, length(y0)), backend)
             vjp   = MadDiff.vector_jacobian_product!(sens; dL_dx, dL_dy)
             for j in 1:n_p
                 Δp     = zeros(n_p); Δp[j] = 1.0
                 s_plus = _solve_exa(p0 .+ h .* Δp; backend, madnlp_opts)
                 dx_fd  = (Vector(MadNLP.variable(s_plus.x)) .- x0) ./ h
                 dy_fd  = (Vector(s_plus.y)                  .- y0) ./ h
-                @test isapprox(vjp.grad_p[j], dot(dL_dx, dx_fd) + dot(dL_dy, dy_fd); atol)
+                @test isapprox(Vector(vjp.grad_p)[j], dot(dL_dx, _to_device(dx_fd, backend)) + dot(dL_dy, _to_device(dy_fd, backend)); atol)
             end
         end
     end
